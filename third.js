cb2adbd6cceb986298b2208ea6784d61486907a3
Hasnain00111 <ahmednecrox@gmail.com>
2024-12-21 00:02:05 -0800

    i am writin a code

---
+++ third.js
@@ -0,0 +1,28 @@
+/*let marks =[50,40,20];
+console.log(marks);
+ let heroes = ["ironman","ramcharan","yash","bilal"]
+ console.log(heroes[0]);
+
+ let pakistaniPlayers = ["babar azam","razwaan","fakhar zaman","shaheen afride","naseem shah"]
+ console.log(pakistaniPlayers[3]);
+ 
+
+
+let keyboardsbottan = ["a","b","c","d","e","f"]
+console.log(keyboardsbottan[3]);*/
+ 
+
+  
+let superstars = ["ironman","thor","hulk","shaktiman","superman"]
+for(let idx = 0; idx<superstars.length; idx++){
+    console.log(idx)
+}
+ let cities = ["gilgit","islamabad","lahore","punjab"]
+ for(let city of cities) {
+    console.log(city);
+ }
+ let marks =
+
+
+
+
