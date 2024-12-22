36ae570260fb69f3c0f79850f39aaeb0e0eea04c
Hasnain00111 <ahmednecrox@gmail.com>
2024-12-21 22:01:16 -0800

    message

--- third.js
+++ third.js
@@ -1,4 +1,4 @@
-/*let marks =[50,40,20];
+let marks =[50,40,20];
 console.log(marks);
  let heroes = ["ironman","ramcharan","yash","bilal"]
  console.log(heroes[0]);
@@ -9,7 +9,7 @@ console.log(marks);
 
 
 let keyboardsbottan = ["a","b","c","d","e","f"]
-console.log(keyboardsbottan[3]);*/
+console.log(keyboardsbottan[3]);
  
 
   
@@ -21,7 +21,57 @@ for(let idx = 0; idx<superstars.length; idx++){
  for(let city of cities) {
     console.log(city);
  }
- let marks =
+ let numbers = [48,90,79,30,67,86];
+ let sum = 0;
+ 
+ for(let val of numbers) {
+    sum += val;
+ }
+ let avg = sum/ numbers.length;
+ console.log('avg numbers of the class = ${avg}' );
+
+
+let foods = ["apple","mengo","banana","pich"]
+
+foods.push("pomogranet");
+console.log(foods);
+
+let footballteam = ["maci","ali","ronaldo","baig"];
+footballteam.push("khan");
+console.log(footballteam);
+
+let students = ["ahmed","sohail","iftikhar","azlan"]
+students.pop("azlan")
+console.log(students);
+ 
+console.log(students.toString());
+
+let teachers = ["salaim","hitash","aman","qasim"]
+teachers.push("gul")
+console.log(teachers);
+
+let friends = ["imaran","farhan","furqan","israr","fazan"]
+friends.pop("fazan");
+console.log(friends);
+
+let singers = ["saaghari","saghar","arjitsingh"]
+let poiets = ["allamaiqbal","bahadurshahzafar","ghalib"]
+let artists = singers.concat(poiets);
+console.log(artists);
+
+let books = ["math","com","phy"]
+books.unshift("urdu")
+console.log(books);
+  let mobiles = ["iphone","sumsung","oppo","infinix"]
+  console.log(mobiles.slice(1,3));
+  
+
+  let evennumbers = [2,4,6,8,10]
+  evennumbers.splice(2,3,12,14)
+  console.log(evennumbers);
+  
+
+
 
 
 
